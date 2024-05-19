e075ebe609ccf78928287d3a91913bf9fefcb16b
KRISHANSHARMA02 <114760812+KRISHANSHARMA02@users.noreply.github.com>
2024-05-19 23:38:22 +0530

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { HiOutlineArrowRight } from "react-icons/hi";
+
+const About = () => {
+  return (
+    <>
+      <section className="about" id="about">
+        <div className="container">
+          <div className="banner">
+            <div className="top">
+              <h1 className="heading">ABOUT US</h1>
+              <p>The only thing we&apos;re serious about is food.</p>
+            </div>
+            <p className="mid">
+            Welcome to our Restaurant Booking System! Our platform is dedicated to enhancing your dining experience by simplifying the reservation process. Whether you're planning a romantic dinner, a family gathering, or a business meeting, our system ensures you can book a table with ease and convenience.
+            </p>
+            <Link to={"/"}>
+              Explore Menu{" "}
+              <span>
+                <HiOutlineArrowRight />
+              </span>
+            </Link>
+          </div>
+          <div className="banner">
+            <img src="about.png" alt="about" />
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default About;
